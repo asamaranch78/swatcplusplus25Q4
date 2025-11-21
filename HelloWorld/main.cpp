08a190783c5fc00ec0614b9751b63a60995db4aa
Inaki Arrondo <inaki.arrondo-cipitria@capgemini.com>
2025-11-21 13:26:17 +0100

    Ejercicio Hello World

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Include the input-output stream library
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Print message to console
+    std::cout << "This is my first C++ program!" << std::endl;  // Print message to console
+    return 0;  // Indicate successful program termination
+}
+
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Notas del curso de c++
+
+# Primer ejercicio HelloWorld
+
+Nos han mandado el codigo para chequear que todo esta como debe ser aprovecho para generar un estructura de proyecto que me permita hacer le resto de ejercicios. 
