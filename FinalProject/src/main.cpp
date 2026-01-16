f5e1ec8f5f2ad8475a4bad30b758574f28e048af
Inaki Arrondo <inaki.arrondo-cipitria@capgemini.com>
2026-01-16 11:01:50 +0100

    Reorganization of main

--- FinalProject/src/main.cpp
+++ FinalProject/src/main.cpp
@@ -10,9 +10,9 @@
 #include "constants.h"
 #include "mainWindow.h"
 #include "keyHandler.h"
-#include "popUpWindow.h"
 
 int main (void);
+void initCurses(void);
 
 int main (void) {
     std::shared_ptr<DataSet> data = std::make_shared<DataSet>();
@@ -20,44 +20,47 @@ int main (void) {
     
 
     initData(data);
+    initCurses();
 
     /*MenuCLI menu;
     while (true) {
         if (!menu.askForSelection()) {return 0;} 
         menu.handleSelection(data);
     }*/
-    initscr();
-    start_color();
-    noecho();
-    keypad(stdscr, TRUE);
-    curs_set(0); // Hide cursor
     centerX = getmaxx(stdscr) / 2;
     windowHeight = getmaxy(stdscr);
 
-    init_pair(MAIN_COLOR, COLOR_WHITE, COLOR_BLACK);
-    init_pair(SELECTED_COLOR, COLOR_BLACK, COLOR_WHITE);
-    init_pair(MENU_COLOR, COLOR_CYAN, COLOR_BLACK);
-    init_pair(ERROR_COLOR, COLOR_WHITE, COLOR_RED);
 
     std::shared_ptr<MainWindow> mainWin;
     mainWin = std::make_shared<MainWindow>(windowHeight, centerX, MAIN_COLOR);
     mainWin->drawConstantPart();
-    mainWin->drawDisplayData(data);
-    mainWin->refresh();
     KeyHandler keyboard(mainWin);
 
     while (true) {
+        mainWin->drawDisplayData(data);
+        mainWin->refresh();
+
+        napms(100);
+
         if (keyboard.listenKeyboard()) {
             keyboard.handleKey();
-
-            mainWin->drawDisplayData(data);
-            mainWin->refresh();
-
-            napms(100);
         }
         else {
             endwin();
             return 0;
         }
     }
+}
+
+void initCurses(void) {
+    initscr();
+    start_color();
+    noecho();
+    keypad(stdscr, TRUE);
+    curs_set(0); // Hide cursor
+    
+    init_pair(MAIN_COLOR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(SELECTED_COLOR, COLOR_BLACK, COLOR_WHITE);
+    init_pair(MENU_COLOR, COLOR_GREEN, COLOR_BLACK);
+    init_pair(ERROR_COLOR, COLOR_WHITE, COLOR_RED);
 }
